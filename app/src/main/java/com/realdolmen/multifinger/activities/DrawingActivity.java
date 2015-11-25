2c7abe409188a832edab5b405d1a91565810d2d7
Dean Wyns <dean.wyns@outlook.com>
2015-11-25 15:10:49 +0100

    Netcode

--- app/src/main/java/com/realdolmen/multifinger/activities/DrawingActivity.java
+++ app/src/main/java/com/realdolmen/multifinger/activities/DrawingActivity.java
@@ -102,7 +102,7 @@ public class DrawingActivity extends RoboActivity implements NumberPicker.OnValu
                 case STROKE_DRAWN:
                     byte[] strokeData = new byte[18];
                     buffer.position(1);
-                    buffer.get(strokeData, 1, 18);
+                    buffer.get(strokeData, 0, 18);
                     StrokeDto strokeDto = conversionUtil.fromBytes(strokeData);
                     graphicsFragment.drawOpponentStroke(strokeDto);
                     break;
